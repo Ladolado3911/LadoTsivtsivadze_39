9b067cb03470d762846299d5c9d1e69f3b045c41
Lado <Tsivtsivadzelado@yahoo.com>
2021-06-23 03:43:22 -0700

    task 2 is done

--- LadoTsivtsivadze_39/Presentation/Scenes/Main/ViewController.swift
+++ LadoTsivtsivadze_39/Presentation/Scenes/Main/ViewController.swift
@@ -12,10 +12,65 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+//        print("1")
+//        DispatchQueue.main.async {
+//            print("2")
+//        }
+//        print("3")
+//        DispatchQueue.main.async {
+//            print("4")
+//        }
+//        print("5")
+//        DispatchQueue.main.async {
+//            print("6")
+//        }
+//        print("7")
     }
     
     @IBAction func onPrint(_ sender: Any) {
+        printNumbers()
     }
     
+    func printNumbers() {
+        print("Fetch 1")
+        
+        DispatchQueue.main.async {
+            print("Fetch 2")
+            DispatchQueue.main.async {
+               print("Fetch 3")
+            }
+            DispatchQueue.main.async {
+                print("Fetch 4")
+            }
+            DispatchQueue.global().sync {
+                print("Fetch 5")
+            }
+        }
+        
+        DispatchQueue.main.async {
+            print("Fetch 8")
+                DispatchQueue.main.async {
+                    print("Fetch 9")
+                        DispatchQueue.global().sync {
+                            print("Fetch 10")
+                        }
+                }
+        }
+        
+        print("Fetch 6")
+        
+        DispatchQueue.main.async {
+            print("Fetch 7")
+                DispatchQueue.global().sync {
+                    print("Fetch 11")
+                        DispatchQueue.global().sync {
+                            print("Fetch 12")
+                                DispatchQueue.main.async {
+                                    print("Fetch 13")
+                                }
+                        }
+                }
+        }
+    }
 }
 
